573d7f9ea4ced8ac3f5d689c0c948ca5990ef919
FrancieleCsantos <francieleconceicaodossantos93@gmail.com>
2023-07-22 18:43:01 -0300

    inicado pagina sobre

--- projeto-final/src/pagesSite/Sobre.jsx
+++ projeto-final/src/pagesSite/Sobre.jsx
@@ -1,6 +1,51 @@
+// import Header from "../components/Header";
+import styles from '../styles/pages/sobre.module.css'
+import Subtitle from '../components/Subtitle'
+import eu from '../assets/Minhaversao.jpeg'
+import Image from '../components/Image';
+
 function Sobre() {
-    return(
-<div>Teste</div>
+
+    return (
+        <>
+            <main className={styles.containerMainSobre}>
+                <section className={styles.containerSobre}>
+                    <div>
+                        <Subtitle content="Conheça um pouco sobre mim" />
+                        <p className={styles.containerSobreTexHello}>Olá,</p><br />
+                        <p className={styles.containerSobreTex}> Aqui quem escreve é Fran Santos.
+                            <br />
+                            Criadora da Rede Mulher.
+                        </p><br />
+
+                        <p className={styles.containerSobreTex}>Comecei meu primeiro relacionamento aos 17 anos, ele era 5 anos mais velho. No início eu acreditava
+                            que o seu comportamento  era normal, que as crises de ciúmes e que regular
+                            minhas vestimentas eram apenas uma forma de demonstrar seu amor por mim.
+                            Ele me fez acreditar que ninguém além dele poderia me amar, me afastou dos meus amigos,
+                            gritava comigo o tempo todo, jogava na minha cara os presentes que me dava, era um
+                            relacionamento baseado no controle e não no amor.
+                        </p><br />
+                        <p className={styles.containerSobreTex}>
+                            Eu vivi um relacionamento abusivo na adolescência, tive dependência
+                            emocional, e te digo caro leitor, foi doloroso sair daquela fase
+                            sozinha. A rede Mulher foi criada com o propósito de conectar mulheres que sofreram violência
+                            ou abuso a instituições que prestam assistência psicológica de forma gratuita.
+                        </p><br />
+                        <p className={styles.containerSobreTex}>
+                            29 anos, formada em Gestão de Recursos Humanos, de Salvador-Ba.
+                            Atualmente estudo analise e desenvolvimento de sistemas,
+                            e estou em transição de carreira para a área de desenvolvimento
+                            de software. Pensando nas diversas mulheres que passam pela mesma situação que vive,
+                            desenvolvi a Rede Mulher com o Intuito de ajuda-las a identificar sinais de um
+                            relacionamento abusivo e para que saibam onde buscar apoio.
+                        </p>
+                    </div>
+                    <div className={styles.containerSobreImg}>
+                        <Image img={eu} altText="Foto da autora, mulher sorrindo com o rosto virado para o céu amirando a natureza" />
+                    </div>
+                </section>
+            </main>
+        </>
     )
 }
 
