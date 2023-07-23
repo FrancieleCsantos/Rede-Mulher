c1465e4db1aff73702b7424acaf567dabb79239a
FrancieleCsantos <francieleconceicaodossantos93@gmail.com>
2023-07-23 18:26:47 -0300

    Iniciado construção da tela Ajuda

--- projeto-final/src/pagesSite/Ajuda.jsx
+++ projeto-final/src/pagesSite/Ajuda.jsx
@@ -1,41 +1,29 @@
 import styles from '../styles/pages/ajuda.module.css'
+import Image from '../components/Image';
+import ajuda from "../assets/fundoAjuda2.png"
+import Subtitle from "../components/Subtitle"
 
 function Ajuda() {
-    return(
-<>
-<main className={styles.containerMainAjuda}>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-    <div>Teste</div>
-</main>
-</>
+    return (
+        <>
+            <main className={styles.containerMainAjuda}>
+                <div className={styles.containerImgAjuda}>
+                    <Image img={ajuda} altText="Grupo de mulheres se braçando" />
+                    <Subtitle content="O objetivo do acolhimento psicológico é promover uma escuta ativa,
+                      para que você possa  falar sobre sentimentos e emoções presentes, momentos e 
+                    vivências com algum ponto de dificuldade, gerando reflexão e autoconhecimento."/>
+                </div>
+{/* <Title/> */}
+                <div className={styles.containerInput}>
+                    <input></input>
+                    <div className={styles.containerCards}>
+
+                    </div>
+                </div>
+
+
+            </main>
+        </>
     )
 }
 
